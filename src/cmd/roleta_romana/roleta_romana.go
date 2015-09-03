517ff5bd87664d524cf0c58ab6b623bf0562f19f
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2015-09-02 21:57:05 -0300

    Tudo funcionando, teste genocida

--- src/cmd/roleta_romana/roleta_romana.go
+++ src/cmd/roleta_romana/roleta_romana.go
@@ -3,13 +3,33 @@ package roleta_romana
 func RoletaRomana(n int, k int, i int) (indice_sobrevivente int, err string) {
 	if n < 1 {
 		err = "n menor que 1"
+		return
 	}
 	if k < 1 {
 		err = "k menor que 1"
+		return
 	}
 	if i < 1 {
 		err = "i menor que 1"
+		return
 	}
-	indice_sobrevivente = 1
+
+	pessoas := make([]int, n)
+	for index := 0; index < n; index++ {
+		pessoas[index] = (index + 1)
+	}
+
+	indice_sobrevivente = roletaRomana(pessoas, k, i-1)
+
 	return
+}
+
+func roletaRomana(sobreviventes []int, k int, i int) int {
+	if len(sobreviventes) == 1 {
+		return sobreviventes[0]
+	} else {
+		quem_morre := (i + k) % len(sobreviventes)
+		sobreviventes = append(sobreviventes[0:quem_morre], sobreviventes[(quem_morre+1):]...)
+		return roletaRomana(sobreviventes, k, quem_morre)
+	}
 }
--- src/cmd/roleta_romana/roleta_romana_test.go
+++ src/cmd/roleta_romana/roleta_romana_test.go
@@ -15,7 +15,6 @@ func TestValidacaoDados(t *testing.T) {
 	if err != "n menor que 1" {
 		t.Errorf("err deve exibir messagem de erro: %s", err)
 	}
-
 	_, err = RoletaRomana(1, 0, 1)
 	if err != "k menor que 1" {
 		t.Errorf("err deve exibir messagem de erro: %s", err)
@@ -23,5 +22,37 @@ func TestValidacaoDados(t *testing.T) {
 	_, err = RoletaRomana(1, 1, 0)
 	if err != "i menor que 1" {
 		t.Errorf("err deve exibir messagem de erro: %s", err)
+	}
+}
+
+func TestMataOPrimeiro(t *testing.T) {
+	indice_sobrevivente, _ := RoletaRomana(2, 1, 2)
+
+	if indice_sobrevivente != 2 {
+		t.Errorf("indice_sobrevivente deveria ser 2, mas foi %d", indice_sobrevivente)
+	}
+}
+
+func TestComCincoPessoas(t *testing.T) {
+	indice_sobrevivente, _ := RoletaRomana(5, 2, 1)
+
+	if indice_sobrevivente != 4 {
+		t.Errorf("indice_sobrevivente deveria ser 4, mas foi %d", indice_sobrevivente)
+	}
+}
+
+func TestDerradeiro(t *testing.T) {
+	indice_sobrevivente, _ := RoletaRomana(10, 1, 1)
+
+	if indice_sobrevivente != 5 {
+		t.Errorf("indice_sobrevivente deveria ser 5, mas foi %d", indice_sobrevivente)
+	}
+}
+
+func TestGenocidio(t *testing.T) {
+	indice_sobrevivente, _ := RoletaRomana(10000, 2, 1)
+
+	if indice_sobrevivente != 2692 {
+		t.Errorf("indice_sobrevivente deveria ser 2692, mas foi %d", indice_sobrevivente)
 	}
 }
